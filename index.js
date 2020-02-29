9faf5ea8279ba43799d2b8ce529a67f772c38945
JiaLiPassion <JiaLi.Passion@gmail.com>
2020-02-29 16:54:42 +0900

    new demo

--- index.js
+++ index.js
@@ -6,23 +6,16 @@ class Test {
 
 window.onload = function() {
   const btn = document.getElementById('btn');
-  btn.addEventListener('click', function() {
-    const div = document.createElement('div');
-    div.innerHTML = 'test div';
-    div['rock'] = new Test(div);
+  const testListener = function() {
     const newBtn = document.createElement('button');
     newBtn.innerText = 'newButton';
-    const listener = function eventListener(e) {
-      const p = e.target.parentNode;
-      // 1. remove button from div will avoid memory leak
-      // p.removeChild(e.target);
-
-      // 2. remove listener from button will not avoid memory leak
-      // e.target.removeEventListener('click', listener);
-      document.body.removeChild(p);
-    };
+    newBtn.rock = new Test(newBtn);
+    const listener = function eventListener(e) {};
     newBtn.addEventListener('click', listener);
-    div.appendChild(newBtn);
-    document.body.appendChild(div);
-  });
+    document.body.appendChild(newBtn);
+    setTimeout(function() {
+      document.body.removeChild(newBtn);
+    }, 5000);
+  };
+  btn.addEventListener('click', testListener);
 };
